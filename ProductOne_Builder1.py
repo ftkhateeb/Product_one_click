66c08c54cc1b8ad73a26334d878a4135eb798e9d
VNET\aelkhate <ahmed.elkhateeb@valeo.com>
2022-11-02 10:34:33 +0200

    added new buttons for -Building HW1 Slave -Openning WinIdea -Flashing APGEn APP -Building APGEn '

--- ProductOne_Builder1.py
+++ ProductOne_Builder1.py
@@ -6,32 +6,48 @@ from tkinter import *
 import os 
 
 root = Tk()
+root.title("Poruduct_ONE_CLICK")
 
 # class for our push buttons in order to avoid code reuse when doing mutliple buttons for canoe, HW1, HW1_2, Winidea, APGEN etc
+# this class is used to hold the data about the button to be created, like the dimesnions, functionality and button name.. etc
 class PushButton:
+    Number_of_buttons = 0
     #Constructor 
-    def __init__ (self, buttonName, fileDirectory, cmd, height = 20 , length = 50):
+    def __init__ (self, buttonName, fileDirectory, cmd, height = 10 , length = 40   ):
+        #static variable that contains the number of buttons 
+        PushButton.Number_of_buttons +=1;
         self.name=buttonName
         self.directory=fileDirectory
         self.command=cmd
         self.Height=height
-        self.Length=length
-        self.TkinterButon_init()
+        self.Length=length 
+        #call the function that initialze and create a tkinter GUI button
+        self.TkinterButonInit()
 
     #function that contains the whole command that will be executed when we click the button 
     def myClick(self):
         os.system('cd {} && {}'.format(self.directory, self.command))
     
-    # initialize new Tkinter button inside our PushButton class
-    def TkinterButon_init:
+    #Create new Tkinter button in the gui
+    def TkinterButonInit(self):
         new_button = Button(root, text = self.name, command = self.myClick, padx = self.Length, pady = self.Height)
-        new_button.pack()
-        root.mainloop()
+        #put the new button in a new raw depending on the number of buttons
+        new_button.grid(row = PushButton.Number_of_buttons)
+        #Pack the button to appear in the gui pp
 
 
 
-
+#### How to create a New Button with a different functionality ?
+## 1-  
+#Push button to build HW1 master
 Build_Hw1_master_Button = PushButton(buttonName = "Build HW1 Master", fileDirectory = 'C:\Projects\Testinggit\DAS\proj1140_gss_das\embedded_env\make_scripts', cmd = 'bootstrap_make.bat /fic= NautilusMaster.all.ren_f1km /hw_target=REN_HW1_TMPL02 -j8' )
-
+#Push Button to build HW1 slave
+Build_Hw1_Gateway_Button = PushButton(buttonName = "Build HW1 Gateway", fileDirectory = 'C:\Projects\Testinggit\DAS\proj1140_gss_das\embedded_env\make_scripts', cmd = 'bootstrap_make.bat /fic= NautilusGateway.all.ren_f1km /hw_target=REN_HW1_TMPL02 -j8' )
+#push Button to open Winidea workspace of HW1
+Winidea_Button = PushButton(buttonName = "OpenWinIdea", fileDirectory = 'C:\Projects\Testinggit\DAS\proj1140_gss_das\embedded_env\\tool\\winidea', cmd = 'ZDAS_3_1_IC5500-REN-HW1.xjrf' )
+Canoe_Button = PushButton(buttonName = "OpenCanoe", fileDirectory = 'C:\Program Files\\Vector CANoe 12.0__2\\Exec64', cmd = 'CANoe64.exe' )
+BuildApgen_Button = PushButton(buttonName = "BuildApgen", fileDirectory = '', cmd = '' )
+FlashApgenApp_Button = PushButton(buttonName = "FlashApgenApp", fileDirectory = '', cmd = '' )
+root.mainloop()
 
 
